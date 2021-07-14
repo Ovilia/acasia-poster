f3e37ab8cb26525d9d395dd5d0ca0c94a4379220
Ovilia <zwl.sjtu@gmail.com>
2021-07-14 11:22:25 +0800

    add template img

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <Tutorial/>
+  <el-row type="flex" class="poster-container" justify="center">
+    <el-col :span="6">
+      <img class="poster-template" src="poster-template.png">
+    </el-col>
+    <el-col :span="6" class="poster-control">
+      this is control
+    </el-col>
+  </el-row>
 </template>
 
 <script lang="ts">
@@ -7,3 +14,25 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style>
+body {
+  background-color: #222;
+}
+
+.poster-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+  .poster-template {
+    height: 100%;
+  }
+
+.poster-control {
+  color: #eee;
+}
+</style>
